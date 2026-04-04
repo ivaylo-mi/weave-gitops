1ae38f8e1211005b3e0b5f1944ff80f3a18e2681
ivaylo-mi <46621272+ivaylo-mi@users.noreply.github.com>
2026-04-04 09:40:27 +0300

    chore(proto): make proto

--- pkg/api/core/core_grpc.pb.go
+++ pkg/api/core/core_grpc.pb.go
@@ -54,47 +54,63 @@ const (
 // *
 // Weave GitOps Core API service definition
 type CoreClient interface {
+	//
 	// GetObject gets data about a single primary object from a cluster.
 	GetObject(ctx context.Context, in *GetObjectRequest, opts ...grpc.CallOption) (*GetObjectResponse, error)
+	//
 	// ListObjects gets data about primary objects.
 	ListObjects(ctx context.Context, in *ListObjectsRequest, opts ...grpc.CallOption) (*ListObjectsResponse, error)
+	//
 	// ListFluxRuntimeObjects lists the flux runtime deployments from a cluster.
 	ListFluxRuntimeObjects(ctx context.Context, in *ListFluxRuntimeObjectsRequest, opts ...grpc.CallOption) (*ListFluxRuntimeObjectsResponse, error)
 	ListFluxCrds(ctx context.Context, in *ListFluxCrdsRequest, opts ...grpc.CallOption) (*ListFluxCrdsResponse, error)
+	//
 	// ListRuntimeObjects lists Weave GitOps runtime components.
 	// Weave GitOps runtime is composed of Flux runtime but also other components
 	// in the ecosystem like TF-controller or Policy Agent.
 	ListRuntimeObjects(ctx context.Context, in *ListRuntimeObjectsRequest, opts ...grpc.CallOption) (*ListRuntimeObjectsResponse, error)
+	//
 	// ListRuntimeCrds lists Weave GitOps runtime components CRDs.
 	// Weave GitOps runtime is composed of Flux runtime but also other components
 	// in the ecosystem like TF-controller or Policy Agent.
 	ListRuntimeCrds(ctx context.Context, in *ListRuntimeCrdsRequest, opts ...grpc.CallOption) (*ListRuntimeCrdsResponse, error)
+	//
 	// GetReconciledObjects returns a list of objects that were created
 	// as a result of reconciling a Flux automation.
 	// This list is derived by looking at the Kustomization or HelmRelease
 	// specified in the request body.
 	GetReconciledObjects(ctx context.Context, in *GetReconciledObjectsRequest, opts ...grpc.CallOption) (*GetReconciledObjectsResponse, error)
+	//
 	// GetChildObjects returns the children of a given object,
 	// specified by a GroupVersionKind.
 	// Not all Kubernets objects have children. For example, a Deployment
 	// has a child ReplicaSet, but a Service has no child objects.
 	GetChildObjects(ctx context.Context, in *GetChildObjectsRequest, opts ...grpc.CallOption) (*GetChildObjectsResponse, error)
+	//
 	// GetFluxNamespace returns with a namespace with a specific label.
 	GetFluxNamespace(ctx context.Context, in *GetFluxNamespaceRequest, opts ...grpc.CallOption) (*GetFluxNamespaceResponse, error)
+	//
 	// ListNamespaces returns with the list of available namespaces.
 	ListNamespaces(ctx context.Context, in *ListNamespacesRequest, opts ...grpc.CallOption) (*ListNamespacesResponse, error)
+	//
 	// ListEvents returns with a list of events
 	ListEvents(ctx context.Context, in *ListEventsRequest, opts ...grpc.CallOption) (*ListEventsResponse, error)
+	//
 	// SyncResource forces a reconciliation of a Flux resource
 	SyncFluxObject(ctx context.Context, in *SyncFluxObjectRequest, opts ...grpc.CallOption) (*SyncFluxObjectResponse, error)
+	//
 	// GetVersion returns version information about the server
 	GetVersion(ctx context.Context, in *GetVersionRequest, opts ...grpc.CallOption) (*GetVersionResponse, error)
+	//
 	// GetFeatureFlags returns configuration information about the server
 	GetFeatureFlags(ctx context.Context, in *GetFeatureFlagsRequest, opts ...grpc.CallOption) (*GetFeatureFlagsResponse, error)
+	//
 	// ToggleSuspendResource suspends or resumes a flux object.
 	ToggleSuspendResource(ctx context.Context, in *ToggleSuspendResourceRequest, opts ...grpc.CallOption) (*ToggleSuspendResourceResponse, error)
+	//
 	// GetSessionLogs returns the logs for a given session
 	GetSessionLogs(ctx context.Context, in *GetSessionLogsRequest, opts ...grpc.CallOption) (*GetSessionLogsResponse, error)
+	//
 	// IsCRDAvailable returns with a hashmap where the keys are the names of
 	// the clusters, and the value is a boolean indicating whether given CRD is
 	// installed or not on that cluster.
@@ -104,8 +120,10 @@ type CoreClient interface {
 	ListPolicies(ctx context.Context, in *ListPoliciesRequest, opts ...grpc.CallOption) (*ListPoliciesResponse, error)
 	// GetPolicy gets a policy by name
 	GetPolicy(ctx context.Context, in *GetPolicyRequest, opts ...grpc.CallOption) (*GetPolicyResponse, error)
+	//
 	// ListPolicyValidations lists policy validations
 	ListPolicyValidations(ctx context.Context, in *ListPolicyValidationsRequest, opts ...grpc.CallOption) (*ListPolicyValidationsResponse, error)
+	//
 	// GetPolicyValidation gets a policy validation by id
 	GetPolicyValidation(ctx context.Context, in *GetPolicyValidationRequest, opts ...grpc.CallOption) (*GetPolicyValidationResponse, error)
 }
@@ -345,47 +363,63 @@ func (c *coreClient) GetPolicyValidation(ctx context.Context, in *GetPolicyValid
 // *
 // Weave GitOps Core API service definition
 type CoreServer interface {
+	//
 	// GetObject gets data about a single primary object from a cluster.
 	GetObject(context.Context, *GetObjectRequest) (*GetObjectResponse, error)
+	//
 	// ListObjects gets data about primary objects.
 	ListObjects(context.Context, *ListObjectsRequest) (*ListObjectsResponse, error)
+	//
 	// ListFluxRuntimeObjects lists the flux runtime deployments from a cluster.
 	ListFluxRuntimeObjects(context.Context, *ListFluxRuntimeObjectsRequest) (*ListFluxRuntimeObjectsResponse, error)
 	ListFluxCrds(context.Context, *ListFluxCrdsRequest) (*ListFluxCrdsResponse, error)
+	//
 	// ListRuntimeObjects lists Weave GitOps runtime components.
 	// Weave GitOps runtime is composed of Flux runtime but also other components
 	// in the ecosystem like TF-controller or Policy Agent.
 	ListRuntimeObjects(context.Context, *ListRuntimeObjectsRequest) (*ListRuntimeObjectsResponse, error)
+	//
 	// ListRuntimeCrds lists Weave GitOps runtime components CRDs.
 	// Weave GitOps runtime is composed of Flux runtime but also other components
 	// in the ecosystem like TF-controller or Policy Agent.
 	ListRuntimeCrds(context.Context, *ListRuntimeCrdsRequest) (*ListRuntimeCrdsResponse, error)
+	//
 	// GetReconciledObjects returns a list of objects that were created
 	// as a result of reconciling a Flux automation.
 	// This list is derived by looking at the Kustomization or HelmRelease
 	// specified in the request body.
 	GetReconciledObjects(context.Context, *GetReconciledObjectsRequest) (*GetReconciledObjectsResponse, error)
+	//
 	// GetChildObjects returns the children of a given object,
 	// specified by a GroupVersionKind.
 	// Not all Kubernets objects have children. For example, a Deployment
 	// has a child ReplicaSet, but a Service has no child objects.
 	GetChildObjects(context.Context, *GetChildObjectsRequest) (*GetChildObjectsResponse, error)
+	//
 	// GetFluxNamespace returns with a namespace with a specific label.
 	GetFluxNamespace(context.Context, *GetFluxNamespaceRequest) (*GetFluxNamespaceResponse, error)
+	//
 	// ListNamespaces returns with the list of available namespaces.
 	ListNamespaces(context.Context, *ListNamespacesRequest) (*ListNamespacesResponse, error)
+	//
 	// ListEvents returns with a list of events
 	ListEvents(context.Context, *ListEventsRequest) (*ListEventsResponse, error)
+	//
 	// SyncResource forces a reconciliation of a Flux resource
 	SyncFluxObject(context.Context, *SyncFluxObjectRequest) (*SyncFluxObjectResponse, error)
+	//
 	// GetVersion returns version information about the server
 	GetVersion(context.Context, *GetVersionRequest) (*GetVersionResponse, error)
+	//
 	// GetFeatureFlags returns configuration information about the server
 	GetFeatureFlags(context.Context, *GetFeatureFlagsRequest) (*GetFeatureFlagsResponse, error)
+	//
 	// ToggleSuspendResource suspends or resumes a flux object.
 	ToggleSuspendResource(context.Context, *ToggleSuspendResourceRequest) (*ToggleSuspendResourceResponse, error)
+	//
 	// GetSessionLogs returns the logs for a given session
 	GetSessionLogs(context.Context, *GetSessionLogsRequest) (*GetSessionLogsResponse, error)
+	//
 	// IsCRDAvailable returns with a hashmap where the keys are the names of
 	// the clusters, and the value is a boolean indicating whether given CRD is
 	// installed or not on that cluster.
@@ -395,8 +429,10 @@ type CoreServer interface {
 	ListPolicies(context.Context, *ListPoliciesRequest) (*ListPoliciesResponse, error)
 	// GetPolicy gets a policy by name
 	GetPolicy(context.Context, *GetPolicyRequest) (*GetPolicyResponse, error)
+	//
 	// ListPolicyValidations lists policy validations
 	ListPolicyValidations(context.Context, *ListPolicyValidationsRequest) (*ListPolicyValidationsResponse, error)
+	//
 	// GetPolicyValidation gets a policy validation by id
 	GetPolicyValidation(context.Context, *GetPolicyValidationRequest) (*GetPolicyValidationResponse, error)
 	mustEmbedUnimplementedCoreServer()
